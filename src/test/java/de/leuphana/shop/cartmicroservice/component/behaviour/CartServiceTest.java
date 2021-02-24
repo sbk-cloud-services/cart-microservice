5bb22b917b7be54d5a2b0e08ed85762ad7feb1a9
Sebastian Meyne <sebastian.meyne@gmail.com>
2021-02-24 20:13:50 +0100

    Wrote tests for use cases in Cart Service

---
+++ src/test/java/de/leuphana/shop/cartmicroservice/component/behaviour/CartServiceTest.java
@@ -0,0 +1,42 @@
+package de.leuphana.shop.cartmicroservice.component.behaviour;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CartServiceTest {
+    private static CartService cartService;
+
+
+    @BeforeAll
+    public void BeforeAll(){
+
+        cartService = new CartServiceImplementation();
+        
+    }
+
+
+
+
+    @Test
+    public void canCartBeCreated(){
+
+        Assertions.assertNotNull(cartService.createCart());
+
+
+    }
+    @Test
+    public void canCartBeRetrieved(){
+        int cartId = cartService.createCart();
+        Assertions.assertNotNull(cartService.getCart(cartId));
+
+    }
+    @Test
+    public void canArticleBeAdded2Cart(){
+
+        int articleId = 1;
+        Assertions.assertTrue(cartService.addArticle2Cart(articleId));
+
+    }
+
+}
