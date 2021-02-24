7364debe1325cc7fcd8bfab48439d748f3fff636
Sebastian Meyne <sebastian.meyne@gmail.com>
2021-02-24 20:47:28 +0100

    Started implementing Cart and CartItem classes

--- src/main/java/de/leuphana/shop/cartmicroservice/component/structure/Cart.java
+++ src/main/java/de/leuphana/shop/cartmicroservice/component/structure/Cart.java
@@ -1,5 +1,18 @@
 package de.leuphana.shop.cartmicroservice.component.structure;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Cart {
+
+    private Set<CartItem> cartItems;
+    private Integer id;
+
+
+    public Cart(){
+        this.cartItems = new HashSet<CartItem>();
+
+        //TODO: automatically assign ID via connector
+    }
     
 }
---
+++ src/main/java/de/leuphana/shop/cartmicroservice/component/structure/CartItem.java
@@ -0,0 +1,8 @@
+package de.leuphana.shop.cartmicroservice.component.structure;
+
+public class CartItem {
+
+    private Integer quantity;
+    private Article article;
+
+}
