738f98cf2af78bfcad540b8a30a64cf5d43f9da1
Sebastian Meyne <sebastian.meyne@gmail.com>
2021-02-28 00:13:59 +0100

    Fixed indentation for CartServiceTest.java

--- src/test/java/de/leuphana/shop/cartmicroservice/component/behaviour/CartServiceTest.java
+++ src/test/java/de/leuphana/shop/cartmicroservice/component/behaviour/CartServiceTest.java
@@ -7,32 +7,29 @@ import org.junit.jupiter.api.Test;
 public class CartServiceTest {
     private static CartService cartService;
 
-
     @BeforeAll
-    public static void BeforeAll(){
+    public static void BeforeAll() {
 
         cartService = new CartServiceImplementation();
-        
+
     }
 
-
-
-
     @Test
-    public void canCartBeCreated(){
+    public void canCartBeCreated() {
 
         Assertions.assertNotNull(cartService.createCart());
 
-
     }
+
     @Test
-    public void canCartBeRetrieved(){
+    public void canCartBeRetrieved() {
         int cartId = cartService.createCart();
         Assertions.assertNotNull(cartService.getCart(cartId));
 
     }
+
     @Test
-    public void canArticleBeAdded2Cart(){
+    public void canArticleBeAdded2Cart() {
 
         int articleId = 1;
         int cartId = 1;
