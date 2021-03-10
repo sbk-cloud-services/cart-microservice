78f1f285e7c36799dc6c00e5e9cf4ce44eab62c1
Sebastian Meyne <sebastian.meyne@gmail.com>
2021-03-10 20:56:49 +0100

    CartRestConnector: ApplicationContext is null Fixes #29

--- src/main/java/de/leuphana/shop/cartmicroservice/connector/CartRestController.java
+++ src/main/java/de/leuphana/shop/cartmicroservice/connector/CartRestController.java
@@ -1,7 +1,5 @@
 package de.leuphana.shop.cartmicroservice.connector;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,35 +12,32 @@ import de.leuphana.shop.cartmicroservice.component.structure.Cart;
 @RestController
 public class CartRestController {
 
-    @Autowired
-    ApplicationContext applicationContext;
-
     @PostMapping("/carts")
     @ResponseBody
-    public Cart createCart(){
-        CartService cartService = (CartService) applicationContext.getBean("cartServiceImplementation");
-
+    public Cart createCart() {
+        CartService cartService = (CartService) CartServiceApplication.getApplicationContext()
+                .getBean("cartServiceImplementation");
         return cartService.createCart();
-
 
     }
 
     @GetMapping("/carts/{id}")
     @ResponseBody
-    public Cart getCart(@PathVariable("id") Integer cartId){
-        CartService cartService = (CartService) applicationContext.getBean("cartServiceImplementation");
+    public Cart getCart(@PathVariable("id") Integer cartId) {
+        CartService cartService = (CartService) CartServiceApplication.getApplicationContext()
+                .getBean("cartServiceImplementation");
 
         return cartService.getCart(cartId);
     }
 
-    
     @PostMapping("/carts/{id}/{article}")
     @ResponseBody
-    public void addArticleToCart(@PathVariable("id") Integer cartId, @PathVariable("article") Integer articleId){
+    public void addArticleToCart(@PathVariable("id") Integer cartId, @PathVariable("article") Integer articleId) {
 
-        CartService cartService = (CartService) applicationContext.getBean("cartServiceImplementation");
+        CartService cartService = (CartService) CartServiceApplication.getApplicationContext()
+                .getBean("cartServiceImplementation");
         cartService.addArticleToCart(articleId, cartId);
 
     }
-    
+
 }
